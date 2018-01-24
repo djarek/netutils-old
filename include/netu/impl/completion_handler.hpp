58922cf4296a6ebbf3593c1e427ac4cbb036d4cd
Damian Jarek <damian.jarek93@gmail.com>
2018-01-24 19:50:33 +0100

    Move completion_handler function definitions to impl/completion_handler.hpp
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

---
+++ include/netu/impl/completion_handler.hpp
@@ -0,0 +1,115 @@
+#ifndef NETU_IMPL_COMPLETION_HANDLER_HPP
+#define NETU_IMPL_COMPLETION_HANDLER_HPP
+
+#include <netu/completion_handler.hpp>
+
+namespace netu
+{
+
+template <typename R, typename... Ts>
+template <typename Handler, class = detail::disable_same_conversion_t<Handler, completion_handler<R(Ts...)>>>
+completion_handler<R(Ts...)>::completion_handler(Handler&& handler)
+{
+    std::tie(hptr_, base_) = detail::allocate_handler<R(Ts...)>(std::forward<Handler>(handler));
+}
+
+template <typename R, typename... Ts>
+completion_handler<R(Ts...)>::completion_handler(completion_handler&& other) noexcept:
+    hptr_{other.hptr_},
+    base_{detail::exchange(other.base_, {})}
+{
+}
+
+template <typename R, typename... Ts>
+completion_handler<R(Ts...)>::~completion_handler()
+{
+    if (base_.manage_)
+    {
+        base_.manage_(hptr_, base_);
+    }
+}
+
+template <typename R, typename... Ts>
+template <typename Handler, class = detail::disable_same_conversion_t<Handler, completion_handler<R(Ts...)>>>
+completion_handler<R(Ts...)>& completion_handler<R(Ts...)>::operator=(Handler&& handler)
+{
+    completion_handler tmp{std::forward<Handler>(handler)};
+    this->swap(tmp);
+    return *this;
+}
+
+template <typename R, typename... Ts>
+completion_handler<R(Ts...)>& completion_handler<R(Ts...)>::operator=(completion_handler&& other) noexcept
+{
+    completion_handler tmp{std::move(other)};
+    this->swap(tmp);
+    return *this;
+}
+
+template <typename R, typename... Ts>
+completion_handler<R(Ts...)>& completion_handler<R(Ts...)>::operator=(std::nullptr_t) noexcept
+{
+    completion_handler tmp;
+    this->swap(tmp);
+    return *this;
+}
+
+template <typename R, typename... Ts>
+void completion_handler<R(Ts...)>::swap(completion_handler& other) noexcept
+{
+    using std::swap;
+    swap(hptr_, other.hptr_);
+    swap(base_, other.base_);
+}
+
+template <typename R, typename... Ts>
+template <typename... Args>
+R completion_handler<R(Ts...)>::invoke(Args&&... args)
+{
+    if (!base_.manage_)
+    {
+        throw std::bad_function_call{};
+    }
+
+    return base_.invoke_(detail::handler_op::invoke, hptr_, base_, std::forward<Args>(args)...);
+}
+
+template <typename R, typename... Ts>
+bool operator==(completion_handler<R(Ts...)> const& lhs, std::nullptr_t) noexcept
+{
+    return lhs.base_.manage_ == nullptr;
+}
+
+template <typename R, typename... Ts>
+bool operator==(std::nullptr_t lhs, completion_handler<R(Ts...)> const& rhs) noexcept
+{
+    return rhs == lhs;
+}
+
+template <typename R, typename... Ts>
+bool operator!=(completion_handler<R(Ts...)> const& lhs, std::nullptr_t rhs) noexcept
+{
+    return !(lhs == rhs);
+}
+
+template <typename R, typename... Ts>
+bool operator!=(std::nullptr_t lhs, completion_handler<R(Ts...)> const& rhs) noexcept
+{
+    return rhs != lhs;
+}
+
+template <typename R, typename... Ts>
+completion_handler<R(Ts...)>::operator bool() const noexcept
+{
+    return static_cast<bool>(base_.manage_);
+}
+
+template <typename R, typename... Ts>
+void swap(completion_handler<R(Ts...)>& lhs, completion_handler<R(Ts...)>& rhs) noexcept
+{
+    return lhs.swap(rhs);
+}
+
+} // namespace netu
+
+#endif // NETU_IMPL_COMPLETION_HANDLER_HPP
