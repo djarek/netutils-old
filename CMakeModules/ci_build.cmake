694ac30f0584c1de70df44b3b3f21ce3260d2210
Damian Jarek <damian.jarek93@gmail.com>
2018-04-18 02:02:50 +0200

    Setup codecov.io coverage measurement
    Test coverage of lines and functions will now be measured and reported
    on every PR.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- CMakeModules/ci_build.cmake
+++ CMakeModules/ci_build.cmake
@@ -7,16 +7,22 @@ set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_USE_LAUNCHERS 1)
 
 set(CTEST_COVERAGE_COMMAND "gcov")
-#set(CTEST_MEMORYCHECK_COMMAND "valgrind")
-#set(CTEST_MEMORYCHECK_TYPE "AddressSanitizer")
+# set(CTEST_MEMORYCHECK_COMMAND "valgrind")
+# set(CTEST_MEMORYCHECK_TYPE "AddressSanitizer")
 
 ctest_start("Continuous")
 ctest_configure()
 ctest_build()
+
 ctest_test(RETURN_VALUE TEST_RESULT)
 if (NOT(${TEST_RESULT} EQUAL 0))
     message(SEND_ERROR "ctest_test returned: ${TEST_RESULT}")
-endif (NOT(${TEST_RESULT} EQUAL 0))
-#ctest_coverage()
-#ctest_memcheck()
-#ctest_submit()
+endif ()
+
+# ctest_coverage(RETURN_VALUE TEST_RESULT BUILD "${CTEST_BINARY_DIRECTORY}")
+# if (NOT(${TEST_RESULT} EQUAL 0))
+#     message(SEND_ERROR "ctest_coverage returned: ${TEST_RESULT}")
+# endif ()
+
+# ctest_memcheck()
+# ctest_submit()
