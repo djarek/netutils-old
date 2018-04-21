beeeb260ea6ce9bb1563766f9095f84680efa220
Damian Jarek <damian.jarek93@gmail.com>
2018-04-21 23:18:47 +0200

    Tidy up CMake scripts
    * Use Boost targets instead of globals
    * Remove unused CTest variables
    * Use consistent formatting in CMake scripts
    * Don't overwrite CMAKE_MODULE_PATH
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,3 @@
 
 set(CTEST_PROJECT_NAME "netutils")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
-set(CTEST_DROP_METHOD "cp")
-#set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/home/damian/projects/netutils/build")
-set(CTEST_DROP_SITE_CDASH TRUE)
