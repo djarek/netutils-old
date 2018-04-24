334f65ac95f682349034475db52069b95ff5e8ea
Damian Jarek <damian.jarek93@gmail.com>
2018-04-24 23:32:43 +0200

    Replace decay with remove_cv_ref_t
    Decay does too much type conversions, removing references and
    cv-qualifiers is enough.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- include/netu/detail/type_traits.hpp
+++ include/netu/detail/type_traits.hpp
@@ -26,10 +26,13 @@ exchange(T& obj, U&& val)
     obj = std::forward<U>(val);
     return old;
 }
+template<typename T>
+using remove_cv_ref_t =
+  typename std::remove_cv<typename std::remove_reference<T>::type>::type;
 
 template<typename From, typename To>
 using disable_conversion_t = typename std::enable_if<
-  !std::is_same<To, typename std::decay<From>::type>::value>::type;
+  !std::is_same<To, remove_cv_ref_t<From>>::value>::type;
 
 } // namespace detail
 } // namespace netu
